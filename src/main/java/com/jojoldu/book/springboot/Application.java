69d1ef5b8671b963ff76ae2a9baa010363bcddf7
leejunmyung <mertyn88@gmail.com>
2019-12-29 22:18:42 +0900

    Spring Boot - JPA 기초 - Tomcat으로 볼수있도록 기본 URL 제공

--- src/main/java/com/jojoldu/book/springboot/Application.java
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -5,6 +5,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+    /*
+    insert into posts(author, content, title) values('이준명', '테스트내용', '테스트제목');
+
+    //메모리로 디비가 실행되므로 시작할때마다 테스트 데이터를 넣어줘야한다.
+
+    curl -XPOST -H "Content-Type: application/json" -d '
+    {
+        "author" : "이준명"
+        ,"title" : "제목입니다."
+        ,"content" : "내용입니다."
+    }
+    ' http://localhost:8080/api/v1/posts
+
+     */
+
     public static void main(String[] args){
         SpringApplication.run(Application.class, args);
     }
