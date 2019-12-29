1928bcfcbf85236689af27115902990040a64bcd
leejunmyung <mertyn88@gmail.com>
2019-12-29 22:17:24 +0900

    Spring Boot - JPA 기초 - Tomcat구동후 Select할수있도록 하기

--- src/main/java/com/jojoldu/book/springboot/web/PostsApiController.java
+++ src/main/java/com/jojoldu/book/springboot/web/PostsApiController.java
@@ -1,10 +1,14 @@
 package com.jojoldu.book.springboot.web;
 
 import com.jojoldu.book.springboot.service.posts.PostsService;
+import com.jojoldu.book.springboot.web.dto.PostsListResponseDto;
+import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
 import com.jojoldu.book.springboot.web.dto.PostsSaveRequestDto;
 import com.jojoldu.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /*
     Controller와 Service에서 @Autowired가 없는데 스프링에서 Bean을 주입받는 방식은 다음과 같다
@@ -16,6 +20,9 @@ import org.springframework.web.bind.annotation.*;
     생성자는 롬복인 @RequiredArgsConstructor에서 해결한다. final이 선언된 모든 필드를 인자값으로 하는 생성자를 롬복에서 대신 생성
     생성자를 직접 쓰지 않고 롬복 어노테이션을 사용하는 이유
         해당 클래스의 의존성 관계가 변경될 때마다 생성자 코드를 계속해서 수정하는 번거로움을 해결하기 위해
+
+
+    https://github.com/jojoldu/freelec-springboot2-webservice/issues/
  */
 
 @RequiredArgsConstructor
@@ -35,5 +42,10 @@ public class PostsApiController {
     @PutMapping("/api/v1/posts/{id}")
     public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
         return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
     }
 }
